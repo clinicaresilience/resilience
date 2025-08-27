fc229f8dd7359227a55d4054755ee07735f86acf
MarcosGAC <marcosgabriel.dev@gmail.com>
2025-08-27 11:21:32 -0300

    configurando cores customizadas da clinica no tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Inicio() {
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-screen ">
       <main className="flex w-full h-full flex-col gap-[32px] row-start-2 items-center sm:items-start"></main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
