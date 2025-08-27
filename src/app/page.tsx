f7ff1c4f7332cbcad340018bbe8208e0f43ce2c1
MarcosGAC <marcosgabriel.dev@gmail.com>
2025-08-27 12:50:36 -0300

    criando cabecalho do da pagina com opcoes da navegacao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,70 @@
 "use client";
 
+import Cabecalho from "@/components/cabecalho";
+import IconeLogo from "./assets/icones/logo.png";
+import Image from "next/image";
+
 export default function Inicio() {
+  const estiloBotao =
+    "w-20 h-8 flex items-center justify-center rounded-3xl cursor-pointer transition-colors duration-500 ease-in-out hover:bg-gradient-to-r hover:from-azul-vivido hover:to-roxo hover:text-white";
+
+  const botoesNavegacao = [
+    { rotulo: "Home", link: "#", ativo: true },
+    { rotulo: "Sobre", link: "#" },
+    { rotulo: "Contato", link: "#" },
+  ];
+
   return (
-    <div className="w-full h-screen ">
-      <main className="flex w-full h-full flex-col gap-[32px] row-start-2 items-center sm:items-start"></main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
+    <div className="w-full h-screen">
+      <Cabecalho className="w-full py-4 px-20 absolute drop-shadow-2xl	shadow">
+        <nav className="flex items-center justify-between">
+          {/* Logo e Título */}
+          <div className="flex items-center gap-8">
+            <Image
+              src={IconeLogo}
+              alt="Logo Clínica Resilience"
+              width={80}
+              height={80}
+            />
+            <h1 className="text-2xl font-bold text-azul-escuro">
+              Clínica Resilience
+            </h1>
+          </div>
+
+          {/* Navegação Principal */}
+          <ul className="flex gap-3">
+            {botoesNavegacao.map(({ rotulo, link, ativo }) => (
+              <li
+                key={rotulo}
+                className={`${estiloBotao} ${
+                  ativo
+                    ? "bg-azul-escuro text-white hover:bg-azul-medio"
+                    : "text-gray-500"
+                }`}
+              >
+                <a href={link}>{rotulo}</a>
+              </li>
+            ))}
+          </ul>
+
+          {/* Ações (Login/Cadastro) */}
+          <ul className="flex items-center gap-3">
+            <li className={`${estiloBotao} bg-azul-escuro text-white `}>
+              Login
+            </li>
+            <span className="h-7 w-px bg-azul-escuro-secundario" />
+            <li className={`${estiloBotao} bg-white text-gray-500`}>
+              Cadastro
+            </li>
+          </ul>
+        </nav>
+      </Cabecalho>
+
+      {/* Conteúdo principal */}
+      <main className="flex flex-col items-center sm:items-start w-full h-full gap-8" />
+
+      {/* Rodapé */}
+      <footer className="flex flex-wrap items-center justify-center gap-6" />
     </div>
   );
 }
